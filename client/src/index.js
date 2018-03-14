70dca8feebb324639a534bdd7286bed1ceb29072
zathershtein <hostel@ukr.net>
2018-03-14 22:07:04 +0200

    Mysql added Check null value

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 function addTask() {
     var taskName = document.getElementById('task-name').value;
     var taskList = document.getElementById('tasks-list');
-
-    taskList.innerHTML += taskName + "<br>" + "<hr>";
+    if (taskName) {
+        taskList.innerHTML += taskName + "<br />" + "<hr />";
+    };
 };
