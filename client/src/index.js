ae4a086ecad14b9ce2690523bac28b76c52dc4af
zathershtein <hostel@ukr.net>
2018-04-21 21:02:48 +0300

    Add project button

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,15 @@ function addTask() {
     var taskList = document.getElementById('tasks-list');
     if (taskName) {
         taskList.innerHTML += taskName + "<br />" + "<hr />";
+        document.getElementById('task-name').value = "";
     };
 };
+
+function addProject() {
+    var projectName = document.getElementById('project-name').value;
+    var projectList = document.getElementById('project-list');
+    if (projectName) {
+        projectList.innerHTML += projectName; //insert html-template
+        document.getElementById('task-name').value = "";
+    };
+}
