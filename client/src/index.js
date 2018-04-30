e972cd66629cd0508ab0554d7298c8e69eddb6e9
zathershtein <hostel@ukr.net>
2018-04-30 10:09:17 +0300

    Focus on issue field Try to add key-handler, not finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
+var taskName = document.getElementById('task-name');
+var taskList = document.getElementById('tasks-list');
+
 function addTask() {
-    var taskName = document.getElementById('task-name').value;
-    var taskList = document.getElementById('tasks-list');
-    if (taskName) {
-        taskList.innerHTML += taskName + "<br />" + "<hr />";
-        document.getElementById('task-name').value = "";
+    if (taskName.value) {
+        taskList.innerHTML += taskName.value + "<br />" + "<hr />";
+        taskName.value = "";
+        taskName.focus();
     };
+};
+
+function enterListener() {
+    taskName.addEventListener('keypress', function(event) {
+        console.log(event.key);
+        if (event.key == "13") {
+            addTask();
+        };
+    });
 };
 
 function addProject() {
